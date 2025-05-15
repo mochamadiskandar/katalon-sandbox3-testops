0d2657bb4c9219cc608e24586a71e7e87145c6c3
Mochamad Iskandar <mochamadiskandar19@gmail.com>
2025-05-15 17:01:51 +0700

    initiate simple project

---
+++ project/sandbox3-testops/Test Suites/Sanity Test.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sanity Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>863e6347-3f3c-4ab1-b269-da65f4625627</testSuiteGuid>
+   <testCaseLink>
+      <guid>2f8e864a-8d75-4ad0-aa37-a16c6d7e82e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_Success</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>

926b519f363d733449fb1101556dfbf9c666aeba
Mochamad Iskandar <mochamadiskandar19@gmail.com>
2025-05-16 10:37:35 +0700

    create test suite

---
+++ project/sandbox3-testops/Test Suites/Positive test.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Positive test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f2f24e5c-37cf-459d-95e5-d627ef4a3ac0</testSuiteGuid>
+   <testCaseLink>
+      <guid>e404eb9f-eca1-40bb-afa5-5e81ca56dea5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_Success</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
